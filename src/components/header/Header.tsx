074e5b7100719287acd78e9747a2660b31b2db30
Abdikadir qulle <wlaaldaadaa33@gmail.com>
2024-04-15 11:13:13 +0300

    header style ✨(#002)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <nav className="flex items-center justify-between px-4 py-2">
+      <h1 className="text-3xl font-bold cursor-pointer">
+        <Link to="/">LearnZone</Link>
+      </h1>
+
+      <nav>
+        <ul className="flex items-center space-x-6">
+          <li className='text-md font-medium '>
+            <Link to="">Home</Link>
+          </li>
+          <li>
+            <Link to="">About</Link>
+          </li>
+          <li>
+            <Link to="">Courses</Link>
+          </li>
+          <li>
+            <Link to="">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="flex items-center gap-4">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded">
+          Sign In
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded">
+          Sign Up
+        </button>
+      </div>
+    </nav>
+  );
 }
 
 export default Header
