c1c1c6ea41c29fa344083b55347877223160095b
Abdikadir qulle <wlaaldaadaa33@gmail.com>
2024-04-17 18:56:44 +0300

    ✨(#003)

--- src/components/compains/Compains.tsx
+++ src/components/compains/Compains.tsx
@@ -22,14 +22,14 @@ const members = [
 
 const Compains = () => {
   return (
-    <div className='flex flex-col gap-10'>
+    <div className='flex flex-col gap-10 bg-tertiary p-4 rounded-md'>
         <div className='flex flex-col space-y-4 md:space-y-3 mt-10'>
             <h1 className='text-md font-medium text-center md:text-lg  z-10'>We collaborate with leading education providers and organisations</h1>
             <div className='flex gap-4 flex-wrap items-center justify-center xl:justify-start'>
                 {LocalBrands.map((brands)=> (
                     <>
                     <div className='' key={brands.id}>
-                        <img src={brands.image} alt="photo" className='w-[5rem] h-[5rem] md:w-[8rem] md:h-[8rem] object-contain'/>
+                        <img src={brands.image} alt="photo" className='w-[5rem] h-[5rem] md:w-[8rem] md:h-[8rem] xl:w-[7.5rem] object-contain'/>
                     </div>
                     </>
                 ))}
@@ -47,7 +47,7 @@ export default Compains;
 
 export const LocalBrand = () => {
     return (
-        <div className='mt-10 flex flex-wrap items-center justify-center md:justify-between gap-4'>
+        <div className=' mt-10 flex flex-wrap items-center justify-center md:justify-between gap-4'>
             {members.map((member) =>(
                 <>
                 <div className='rounded-md px-10 py-5 flex flex-col items-center justify-center bg-primaryBlue '>
