dce0171aa7349e1b58c798f048db3e2eb8241e89
Abdikadir qulle <wlaaldaadaa33@gmail.com>
2024-04-19 21:22:21 +0300

    test

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,8 +7,8 @@ const Menu = () => {
       <div className="flex flex-col items-center md:items-start space-y-16">
         <div className='flex flex-col gap-4'>
         <h1 className="relative font-bold text-5xl text-secondary leading-snug text-center md:text-start">
-          Getting <span className="text-primary ">Quality</span> <br /> Education
-          Is Now  More <span className="text-primary">Easy</span>
+          Getting <span className="text-primary font-bold">Quality</span> <br /> Education
+          Is Now  More <span className="text-primary font-bold">Easy</span>
         </h1>
 
         <p className="text-md font-light text-center md:text-start">
@@ -18,7 +18,7 @@ const Menu = () => {
         </p>
         </div>
         <div className="flex flex-col md:flex-row gap-4 items-center  w-full">
-          <button className="bg-primaryBlue w-full  md:w-1/2 text-center font-medium rounded-full py-3 px-12 text-white text-lg text-nowrap">
+          <button className="bg-primaryBlue hover:bg-blue-600 w-full  md:w-1/2 text-center font-medium rounded-full py-3 px-12 text-white text-lg text-nowrap">
             Join for Free.
           </button>
           <button className="bg-secondary border-2 hover:bg-blue-100 border-blue-400 w-full  text-center font-medium rounded-full py-3 px-12 text-secondary text-lg text-nowrap">
