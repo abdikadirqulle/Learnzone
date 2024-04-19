372e0ab9760843285517651e9e223599c62d0b7d
Abdikadir qulle <wlaaldaadaa33@gmail.com>
2024-04-19 15:22:10 +0300

    adding footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,39 @@
+import "./footer.css"
+
+import { FaFacebook ,FaInstagram ,FaTwitter, FaLinkedin,FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="">Footer</div>
-  )
+    <footer className="footer">
+      <section className="">
+        <div className="share">
+          <a href="#" className="fab fa-facebook - f">
+            <FaFacebook />
+          </a>
+          <a href="#" className="fab fa-twitter">
+            <FaTwitter />
+          </a>
+          <a href="#" className="fab fa-linkedin">
+            <FaLinkedin />
+          </a>
+          <a href="#" className="fab fa-instagram">
+            <FaInstagram />
+          </a>
+          <a href="#" className="fab fa-youtube">
+            <FaYoutube />
+          </a>
+        </div>
+
+        <div className="credit">
+          &copy; copyright @ 2024 by{" "}
+          <span style={{}} className="text-primary ">learn zone</span> | all rights
+          reserved!
+        </div>
+      </section>
+    </footer>
+  );
+
+  //color: rgb(28, 28, 184)
 }
 
 export default Footer
